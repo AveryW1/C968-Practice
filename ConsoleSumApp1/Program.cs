b3f9931f97baaff74b4cd869c413a1d8311eb15d
Avery <33632634+AveryW1@users.noreply.github.com>
2021-03-12 13:31:57 -0800

    Sum app
    sum app to test push/pull

---
+++ ConsoleSumApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleSumApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int sum;
+
+            Console.Write("Enter the first Interger: ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second Interger: ");
+            number2 = int.Parse(Console.ReadLine());
+            sum = number1 + number2;
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}
